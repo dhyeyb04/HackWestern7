280d970d1c4f8dbc6cf8eb63298335dbd18ff1f5
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-11-21 03:29:22 -0500

    Some working endpoints

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 var fs = require('fs');
 let data = fs.readFileSync('topicList.json');
 let topics = JSON.parse(data);
+let mans = fs.readFileSync('userList.json');
+let users = JSON.parse(mans);
 const port = 3000
 
 app.use(express.static(__dirname + '/templates'));
@@ -35,4 +37,56 @@ app.get('/topics', (req, res) => {
   res.send(output);
 });
 
-//JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(i);
+app.get('/users', (req, res) => {
+  let output = [];
+
+  for (var i = 0; i < users.length; i++) {
+    output.push({
+      email: users[i].email,
+      password: users[i].password,
+      name: users[i].name
+    });
+  }
+
+  res.send(output);
+});
+
+app.get('/topics/:event', (req, res) => {
+  let output = [];
+
+  for (var i = 0; i < topics.length; i++) {
+    if (topics[i].event === req.params.event) {
+      output.push({
+        event: topics[i].event,
+        category: topics[i].category,
+        description: topics[i].description,
+      });
+    }
+  }
+
+  if (output.length == 0) {
+    return res.status(404).send("The course with the given subject code was not found.");
+  }
+  
+  res.send(output);
+});
+
+app.get('/topics/:event/posts', (req, res) => {
+  let output = [];
+
+  for (var i = 0; i < topics.length; i++) {
+    if (topics[i].event === req.params.event) {
+      output.push({
+        event: topics[i].event,
+        category: topics[i].category,
+        description: topics[i].description,
+      });
+    }
+  }
+
+  if (output.length == 0) {
+    return res.status(404).send("The course with the given subject code was not found.");
+  }
+
+  res.send(output);
+});
