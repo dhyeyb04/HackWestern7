9601d2c29984b3b6069de046bcab85aa7ec59bb5
Harsh Patel <harsh@Harshs-MacBook-Pro.local>
2020-11-21 08:12:42 -0500

    updTEDFEATUREDPAGE
    HI

--- templates/main.js
+++ templates/main.js
@@ -4,11 +4,57 @@ console.log("Hello");
 function addTopic(){
 	console.log("Adding New Topic");
 	var topicsHolder = document.getElementById("TopicsHolder");
-	var newTopic = document.createElement("Div");
-	newTopic.id = "topic";
-	newTopic.className = "col-sm-6";
-	var newTopicText = document.createElement("H3");
-	newTopicText.innerHTML = "Hello World Again";
-	topicsHolder.appendChild(newTopic);
-	newTopic.appendChild(newTopicText);
+
+	var newTopicCard = document.createElement("Div");
+	newTopicCard.className = "card";
+	newTopicCard.id = "topic";
+
+	var newCardHeader = document.createElement("Div");
+	newCardHeader.className = "card-header";
+	newCardHeader.innerHTML = "Topic Title";
+
+	newTopicCard.appendChild(newCardHeader);
+
+	var newCardBody = document.createElement("Div");
+	newCardBody.className = "card-body";
+	newCardBody.id = "cardTopicBody";
+
+	newTopicCard.appendChild(newCardBody);
+
+	var newTopicPost = document.createElement("Div");
+	newTopicPost.className = "card";
+	newTopicPost.id = "topicPost";
+
+	var newTopicPostBody = document.createElement("Div");
+	newTopicPostBody.className = "card-body";
+	newTopicPostBody.id = "topicPostBody";
+
+	newCardBody.appendChild(newTopicPost);
+	newTopicPost.appendChild(newTopicPostBody);
+
+	var postTitle = document.createElement("h5");
+	postTitle.className = "card-title";
+	postTitle.innerHTML = "Sample Post Title";
+	var postSubtitle = document.createElement("h6");
+	postSubtitle.className = "card-subtitle mb-2 text-muted";
+	postSubtitle.innerHTML = "Sample Post subtitle";
+	var postDesc = document.createElement("p");
+	postDesc.className = "card-text";
+	postDesc.innerHTML = "Some quick example text to build on the post title and make up the bulk of the post's content.";
+	var postLink1 = document.createElement("a");
+	postLink1.className = "card-link";
+	postLink1.innerHTML = "Join";
+	var postLink2 = document.createElement("a");
+	postLink2.className = "card-link";
+	postLink2.innerHTML = "Num of joinned users";
+
+	newTopicPostBody.appendChild(postTitle);
+	newTopicPostBody.appendChild(postSubtitle);
+	newTopicPostBody.appendChild(postDesc);
+	newTopicPostBody.appendChild(postLink1);
+	newTopicPostBody.appendChild(postLink2);
+
+	topicsHolder.appendChild(newTopicCard);
+
+
 }
