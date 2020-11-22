568fe9832326e94169a3ba06d9767fcfc9c67e14
Harsh Patel <harsh@Harshs-MacBook-Pro.local>
2020-11-21 23:26:32 -0500

    3 functions to go
    almost donzooo

--- templates/main.js
+++ templates/main.js
@@ -1,5 +1,9 @@
 window.onload = checkUser();
 
+function test(){
+	window.location.href = "/";
+}
+
 function checkUser(){
 	url = window.location.href;
 	email = url.split("=");
@@ -52,68 +56,6 @@ function checkUser(){
 }
 
 
-function addTopic(desc,eventName,tag){
-	var topicsHolder = document.getElementById("TopicsHolder");
-
-	var newTopicCard = document.createElement("Div");
-	newTopicCard.className = "card";
-	newTopicCard.id = "topic";
-
-	var newCardHeader = document.createElement("Div");
-	newCardHeader.className = "card-header";
-	newCardHeader.innerHTML = "Topic Title";
-
-	newTopicCard.appendChild(newCardHeader);
-
-	var newCardBody = document.createElement("Div");
-	newCardBody.className = "card-body";
-	newCardBody.id = "cardTopicBody";
-
-	newTopicCard.appendChild(newCardBody);
-
-	var newTopicPost = document.createElement("Div");
-	newTopicPost.className = "card";
-	newTopicPost.id = "topicPost";
-
-	var newTopicPostBody = document.createElement("Div");
-	newTopicPostBody.className = "card-body";
-	newTopicPostBody.id = "topicPostBody";
-
-	newCardBody.appendChild(newTopicPost);
-	newTopicPost.appendChild(newTopicPostBody);
-
-	var postTitle = document.createElement("h5");
-	postTitle.className = "card-title";
-	postTitle.innerHTML = eventName;
-	var postSubtitle = document.createElement("h6");
-	postSubtitle.className = "card-subtitle mb-2 text-muted";
-	postSubtitle.innerHTML = "Sample Post subtitle";
-	var postDesc = document.createElement("p");
-	postDesc.className = "card-text";
-	postDesc.innerHTML = desc;
-	var postLink1 = document.createElement("a");
-	postLink1.className = "card-link";
-	postLink1.id = "connectUser";
-	postLink1.innerHTML = "Join";
-
-	var postLink2 = document.createElement("a");
-	postLink2.className = "card-link";
-	postLink2.innerHTML = "Followers";
-
-	newTopicPostBody.appendChild(postTitle);
-	newTopicPostBody.appendChild(postSubtitle);
-	newTopicPostBody.appendChild(postDesc);
-	newTopicPostBody.appendChild(postLink1);
-	newTopicPostBody.appendChild(postLink2);
-
-	topicsHolder.appendChild(newTopicCard);
-
-	postLink1.addEventListener("click", function(){
-  		console.log("Harsh is actually the best");
-	});
-
-}
-
 function newTopic(name){
 	var topicsHolder = document.getElementById("TopicsHolder");
 
@@ -125,7 +67,12 @@ function newTopic(name){
 	newCardHeader.className = "card-header";
 	newCardHeader.innerHTML = name;
 
+	var addButton = document.createElement("Button");
+	addButton.id = "addEventsButton";
+	addButton.innerHTML = "Create Event";
+
 	newTopicCard.appendChild(newCardHeader);
+	newCardHeader.appendChild(addButton);
 
 	var newCardBody = document.createElement("Div");
 	newCardBody.className = "card-body";
